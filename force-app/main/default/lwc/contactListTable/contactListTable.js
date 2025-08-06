b6ea3e2780956595f0c414a737a0da342cf086f8
Ujwal KC <ujwalkcsps@gmail.com>
2025-08-06 05:39:09 +0000

    pagination working

--- force-app/main/default/lwc/contactListTable/contactListTable.js
+++ force-app/main/default/lwc/contactListTable/contactListTable.js
@@ -4,6 +4,11 @@ import getAllContacts from '@salesforce/apex/EmployeeController.getAllContacts';
 export default class ContactListTable extends LightningElement {
     @track Contacts;
     @track error;
+    @track totalPages;
+    @track pageSize = 5;
+    @track AllPage = [];
+    @track perPage = [];
+    @track currentPage = 1;
 
     columns = [
         { label: 'Name', fieldName: 'Name' },
@@ -12,15 +17,58 @@ export default class ContactListTable extends LightningElement {
     ];
 
     @wire(getAllContacts)
-    WiredContact({error, data}) {
+    WiredContact({ error, data }) {
         if (data) {
             this.Contacts = data;
             this.error = undefined;
             console.log('contacts: ', JSON.stringify(this.Contacts, null, 2));
-        }
-        else {
+            
+            this.totalPages = Math.ceil(data.length / this.pageSize);
+            this.AllPage = [];
+
+            for (let i = 0; i < data.length; i += this.pageSize) {
+                this.AllPage.push(data.slice(i, i + this.pageSize));
+            }
+
+            this.setPage(1);
+        } else {
             this.error = error;
             this.Contacts = undefined;
         }
+    }
+
+    setPage(pageNumber) {
+        if (pageNumber < 1 || pageNumber > this.totalPages) return;
+
+        this.currentPage = pageNumber;
+        this.perPage = this.AllPage[pageNumber - 1];
+    }
+
+    handleNext() {
+        if (this.currentPage < this.totalPages) {
+            this.setPage(this.currentPage + 1);
+        }
+    }
+
+    handlePrev() {
+        if (this.currentPage > 1) {
+            this.setPage(this.currentPage - 1);
+        }
+    }
+
+    get disablePrev() {
+        return this.currentPage === 1;
+    }
+
+    get disableNext() {
+        return this.currentPage === this.totalPages;
+    }
+
+    get currentPageNumber() {
+        return this.currentPage;
+    }
+
+    get totalPageNumber() {
+        return this.totalPages;
     }
 }
